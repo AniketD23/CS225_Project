d9a393c329d3072e9c67c542f76afed81865bea1
dsding2 <dsding2@illinois.edu>
2022-11-18 04:13:49 +0000

    function signatures for a star

---
+++ code/a_star.cpp
@@ -0,0 +1,5 @@
+#include "a_star.h"
+
+vector<string> A_Star::shortestPath(string start, string end, DataProcessor& data) {
+    
+}
---
+++ code/a_star.h
@@ -0,0 +1,7 @@
+#include "data_processing.h"
+#include <string>
+#include <vector>
+using namespace std;
+class A_Star {
+    static vector<string> shortestPath(string start, string end, DataProcessor& data);
+};
