0d71e550fbbd62c2a2870f29b946af6cfa43e1a6
em-beth <emilygreetis@hotmail.com>
2022-11-17 22:27:10 -0600

    Prim headerfile

---
+++ code/prim.h
@@ -0,0 +1,9 @@
+#include "data_processing.h"
+class Prim {
+ private:
+  // minimum spanning tree
+
+ public:
+  // default constructor
+  // params: string start, string end, DataProcessor data
+};
