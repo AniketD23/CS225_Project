49d90ecd57478528ced6617c55a1f82daad7e9e6
dsding2 <dsding2@illinois.edu>
2022-12-11 22:18:10 +0000

    basic input verification to main

--- entry/main.cpp
+++ entry/main.cpp
@@ -7,10 +7,28 @@
 #include "bfs.h"
 
 int main(int argc, char** argv) {
-  DataProcessor d("../data/movies.dat", "../lists/avg_adj_list_.txt");
+  if (argc < 2) {
+    std::cout << "Too few arguments. Remember, use \"algorithm name\" \"at least one movie argument...\"" << std::endl;
+    return 0;
+  }
 
-  // UserInput ui(argv);
-  // std::cout << ui.getTitle1() << std::endl;
-  // std::cout << ui.getTitle2() << std::endl;
+  if (argv[0] == "A_star") {
+    if (argc < 3) {
+      std::cout << "Too few arguments for A_star. Remember, A_star takes two movie arguments" << std::endl;
+      return 0;
+    }
+  } else if (argv[0] == "BFS") {
+    if (argc < 3) {
+      std::cout << "Too few arguments for BFS. Remember, BFS takes two movie arguments" << std::endl;
+      return 0;
+    }
+  } else if (argv[0] == "Prim"){
+    
+  } else {
+    std::cout << "Unknown algorithm. Remember, the first argument should be either \"A_star\", \"BFS\", or \"Prim\"." << std::endl;
+    return 0;
+  }
+
+  DataProcessor d("../data/movies.dat", "../lists/avg_adj_list_.txt");
   return 0;
 }
