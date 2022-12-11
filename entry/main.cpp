1a89c07a59eb5ec90b5aa409727367094f0b7f07
dsding2 <dsding2@illinois.edu>
2022-12-11 22:21:35 +0000

    small main fix

--- entry/main.cpp
+++ entry/main.cpp
@@ -7,22 +7,22 @@
 #include "bfs.h"
 
 int main(int argc, char** argv) {
-  if (argc < 2) {
+  if (argc < 3) {
     std::cout << "Too few arguments. Remember, use \"algorithm name\" \"at least one movie argument...\"" << std::endl;
     return 0;
   }
 
-  if (argv[0] == "A_star") {
-    if (argc < 3) {
+  if (argv[1] == "A_star") {
+    if (argc < 4) {
       std::cout << "Too few arguments for A_star. Remember, A_star takes two movie arguments" << std::endl;
       return 0;
     }
-  } else if (argv[0] == "BFS") {
-    if (argc < 3) {
+  } else if (argv[1] == "BFS") {
+    if (argc < 4) {
       std::cout << "Too few arguments for BFS. Remember, BFS takes two movie arguments" << std::endl;
       return 0;
     }
-  } else if (argv[0] == "Prim"){
+  } else if (argv[1] == "Prim"){
     
   } else {
     std::cout << "Unknown algorithm. Remember, the first argument should be either \"A_star\", \"BFS\", or \"Prim\"." << std::endl;
