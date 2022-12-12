6e76681f584f20960d0963f751cb373cbe8c189b
em_beth <emilygreetis@hotmail.com>
2022-12-12 23:24:27 +0000

    Prim's user entry

--- code/user_input.cpp
+++ code/user_input.cpp
@@ -1,21 +1,24 @@
 #include "user_input.h"
+
+#include <chrono>
 #include <cstring>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <ostream>
 #include <sstream>
-#include "prim.h"
+
 #include "a_star.h"
 #include "bfs.h"
-#include <iomanip>
-#include <chrono>
+#include "prim.h"
 
-void UserInput::runBFS(DataProcessor& d, std::string start, std::string end, std::string output_filename) {
+void UserInput::runBFS(DataProcessor& d, std::string start, std::string end,
+                       std::string output_filename) {
   output_filename = "../out/" + output_filename;
   std::cout << "\nInitialize bfs" << std::endl;
   BFS bfs(d);
-  
+
   std::cout << "Perform bfs" << std::endl;
   bfs.breadthFirst(start, end);
 
@@ -33,43 +36,79 @@ void UserInput::runBFS(DataProcessor& d, std::string start, std::string end) {
   runBFS(d, start, end, "BFS_out.txt");
 }
 
-void UserInput::runA_star(DataProcessor& d, std::string start, std::string end, std::string output_filename) {
+void UserInput::runA_star(DataProcessor& d, std::string start, std::string end,
+                          std::string output_filename) {
   output_filename = "../out/" + output_filename;
-  
+
   std::cout << "Running A_star\n" << std::endl;
   auto out = A_Star::shortestPath(start, end, d);
 
   int table_width = 60;
-  std::cout << "Movie Title" << std::setw(table_width - 11) << "\"Distance\" from start" << std::endl;
+  std::cout << "Movie Title" << std::setw(table_width - 11)
+            << "\"Distance\" from start" << std::endl;
 
   for (auto p : out) {
     if (p.first.size() > 40) {
       p.first = p.first.substr(0, 40) + "...";
     }
-    std::cout << p.first << std::setw(table_width - p.first.size()) << p.second << std::endl;
+    std::cout << p.first << std::setw(table_width - p.first.size()) << p.second
+              << std::endl;
   }
 
   std::ofstream os;
   os.open(output_filename, std::ofstream::out | std::ofstream::trunc);
-  os << "Movie Title" << std::setw(table_width - 11) << "\"Distance\"" << "\n";
+  os << "Movie Title" << std::setw(table_width - 11) << "\"Distance\""
+     << "\n";
   for (auto p : out) {
     if (p.first.size() > 40) {
       p.first = p.first.substr(0, 40) + "...";
     }
-    os << p.first << std::setw(table_width - p.first.size()) << p.second << "\n";
+    os << p.first << std::setw(table_width - p.first.size()) << p.second
+       << "\n";
   }
   std::cout << "A_star written to file at " << output_filename << std::endl;
 }
 
-void UserInput::runA_star(DataProcessor& d, std::string start, std::string end) {
-  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-  // cout << "F runtime=" << measure<>::duration(runA_star(d, start, end, "A_star_out.txt")).count() << "ms";
+void UserInput::runA_star(DataProcessor& d, std::string start,
+                          std::string end) {
+  std::chrono::steady_clock::time_point begin =
+      std::chrono::steady_clock::now();
+  // cout << "F runtime=" << measure<>::duration(runA_star(d, start, end,
+  // "A_star_out.txt")).count() << "ms";
   runA_star(d, start, end, "A_star_out.txt");
   std::chrono::steady_clock::time_point done = std::chrono::steady_clock::now();
-  std::cout << "\nA_star running time (sec) = " <<  (std::chrono::duration_cast<std::chrono::microseconds>(done - begin).count()) /1000000.0  <<std::endl;
+  std::cout << "\nA_star running time (sec) = "
+            << (std::chrono::duration_cast<std::chrono::microseconds>(done -
+                                                                      begin)
+                    .count()) /
+                   1000000.0
+            << std::endl;
 }
 
-void UserInput::runPrim(DataProcessor& d, std::string start, std::string output_filename) {
+void UserInput::runPrim(DataProcessor& d, std::string start,
+                        std::string output_filename) {
+  output_filename = "../out/" + output_filename;
+  std::cout << "Running Prim (estimated load time: 1-3 minutes) \n"
+            << std::endl;
+  auto result_map = Prim::findMST(d, start);
+  std::cout << "How many movie reccomendations would you like to see?\n"
+            << std::endl;
+  size_t num_movies = result_map.size();
+  std::cout << "Enter a number from 3 - " << num_movies << '\n' << std::endl;
+  int nodes;
+  std::cin >> nodes;
+  if (nodes < 3 || nodes > int(num_movies)) {
+    std::cout << "Please enter a valid number from 3 -" << num_movies << '\n'
+              << std::endl;
+    return;
+  }
+  auto out = Prim::BFTraversalOfMST(start, nodes, result_map, d);
+  std::ofstream os;
+  os.open(output_filename, std::ofstream::out | std::ofstream::trunc);
+  for (string movie : out) {
+    os << movie << '\n';
+  }
+  std::cout << "Prim's written to file at " << output_filename << std::endl;
 }
 
 void UserInput::runPrim(DataProcessor& d, std::string start) {
--- entry/main.cpp
+++ entry/main.cpp
@@ -1,25 +1,33 @@
+#include <string.h>
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <string.h>
+
 #include "data_processing.h"
 #include "user_input.h"
 
 int main(int argc, char** argv) {
   if (argc < 3) {
-    std::cout << "Too few arguments. Remember, use [\"algorithm name\" \"movie 1\" \"movie 2\"]" << std::endl;
+    std::cout << "Too few arguments. Remember, use [\"algorithm name\" \"movie "
+                 "1\" \"movie 2\" \"output file name\"]"
+              << std::endl;
     return 0;
   }
   std::string movie_id_file = "../data/movies.dat";
   std::string adj_list_file = "../lists/avg_adj_list_.txt";
   if (!strcmp(argv[1], "A_star")) {
     if (argc < 4) {
-      std::cout << "Too few arguments for A_star. Remember, A_star takes two movie arguments." << std::endl;
+      std::cout << "Too few arguments for A_star. Remember, A_star takes two "
+                   "movie arguments."
+                << std::endl;
       return 0;
-    } 
+    }
     DataProcessor d(movie_id_file, adj_list_file);
     if (d.titleToID(argv[2]) == -1 || d.titleToID(argv[3]) == -1) {
-      std::cout << "One of the movies passed is not in the dataset. Remember to be strict with capitalization." << std::endl;
+      std::cout << "One of the movies passed is not in the dataset. Remember "
+                   "to be strict with capitalization."
+                << std::endl;
       return 0;
     }
     if (argc < 5) {
@@ -31,12 +39,16 @@ int main(int argc, char** argv) {
 
   } else if (!strcmp(argv[1], "BFS")) {
     if (argc < 4) {
-      std::cout << "Too few arguments for BFS. Remember, BFS takes two movie arguments." << std::endl;
+      std::cout << "Too few arguments for BFS. Remember, BFS takes two movie "
+                   "arguments."
+                << std::endl;
       return 0;
     }
     DataProcessor d(movie_id_file, adj_list_file);
     if (d.titleToID(argv[2]) == -1 || d.titleToID(argv[3]) == -1) {
-      std::cout << "One of the movies passed is not in the dataset. Remember to be strict with capitalization." << std::endl;
+      std::cout << "One of the movies passed is not in the dataset. Remember "
+                   "to be strict with capitalization."
+                << std::endl;
       return 0;
     }
     if (argc < 5) {
@@ -45,10 +57,12 @@ int main(int argc, char** argv) {
       UserInput::runBFS(d, argv[2], argv[3], argv[4]);
     }
     return 0;
-  } else if (!strcmp(argv[1], "Prim")){
+  } else if (!strcmp(argv[1], "Prim")) {
     DataProcessor d(movie_id_file, adj_list_file);
     if (d.titleToID(argv[2]) == -1) {
-      std::cout << "One of the movies passed is not in the dataset. Remember to be strict with capitalization." << std::endl;
+      std::cout << "One of the movies passed is not in the dataset. Remember "
+                   "to be strict with capitalization."
+                << std::endl;
       return 0;
     }
     if (argc < 4) {
@@ -57,8 +71,9 @@ int main(int argc, char** argv) {
       UserInput::runPrim(d, argv[2], argv[3]);
     }
   } else {
-    std::cout << "Unknown algorithm. Remember, the first argument should be either \"A_star\", \"BFS\", or \"Prim\"." << std::endl;
+    std::cout << "Unknown algorithm. Remember, the first argument should be "
+                 "either \"A_star\", \"BFS\", or \"Prim\"."
+              << std::endl;
     return 0;
   }
-
 }
